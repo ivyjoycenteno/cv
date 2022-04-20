15facf4c3d9c669dc5e7663b3265884ea3bf2f79
Rij Nest <rijnest@Rijs-MacBook-Pro.local>
2022-04-20 11:18:04 +0800

    style updates and added certification

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ $(function () {
   $('#copyright').text(new Date().getFullYear());
   $('.nav-mobile .trigger i').click(() => {
     $('.mn-mobile-menu').slideToggle();
-    // $('ul ul').css('display', 'none');
   });
 
   $('.mn-mobile-menu ul li').click(() => {
     $('.mn-mobile-menu').slideUp();
-    $(this).find('ul').slideToggle();
+    // $(this).find('ul').slideToggle();
   });
 });
