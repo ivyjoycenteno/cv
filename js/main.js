8ee3ff56f3b9566c6274cbab913ac850114db88e
Rij Nest <rijnest@Rijs-MacBook-Pro.local>
2022-04-21 13:18:00 +0800

    style and content updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 $(function () {
   $('#copyright').text(new Date().getFullYear());
+
+  handleClick = (elem, e) => {
+    $('.ij-modal').show()
+    $('.ij-modal .ij-content').html($(elem).clone())
+    e.stopPropagation();
+  }
+
   $('.nav-mobile .trigger i').click(() => {
-    $('.mn-mobile-menu').slideToggle();
-  });
+    $('.mn-mobile-menu').slideToggle()
+  })
 
   $('.mn-mobile-menu ul li').click(() => {
-    $('.mn-mobile-menu').slideUp();
-    // $(this).find('ul').slideToggle();
-  });
+    $('.mn-mobile-menu').slideUp()
+  })
+
+  $('.ij-wrap').click((e) => {
+    e.stopPropagation()
+  })
+
+  $('.mn-wrap .ij-close').click(() => {
+    $('.ij-modal').hide()
+  })
+
+  $(document).click(() => {
+    $(".ij-modal").hide()
+  })
 });
